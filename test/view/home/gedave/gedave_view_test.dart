4d4f7000227f7513340ff383ad12825d480bafb6
Caio Ramos <caio.pfn@hotmail.com>
2023-04-05 10:58:05 -0300

    Excluindo os testes

--- test/view/home/gedave/gedave_view_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:gedave/view/home/gedave/gedave_view.dart';
-
-void main() {
-  testWidgets("Localiza os widgets na Gedave", (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: GedaveView()));
-  });
-}
--- test/view/home/gedave/gedave_widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:gedave/view/home/gedave/servicos/consultas/retorno_consultas/consultas_widget.dart';
-
-void main() {
-  testWidgets("Testa animação consulta", (WidgetTester tester) async {
-    tester.pumpWidget(MaterialApp(home: Scaffold(body: AnimacaoConsulta())));
-  });
-}
